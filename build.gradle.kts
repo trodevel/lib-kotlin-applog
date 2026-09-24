e5c328ff8e8584bff9608cedcbacf51e1cbea5c7
trodevel <trodevel@gmail.com>
2026-09-24 22:39:31 +0200

    build (gradle,agp): update to Gradle 9.5.0, AGP 9.3.3 (#2)
    * build (grande,agp): update to Gradle 9.5.0, AGP 9.3.3
    
    * incremented version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
 }
 
 group = "com.trodevel.applog"
-version = "1.1.0"
+version = "1.2.0"
 
 android {
     namespace = "com.trodevel.applog"
@@ -14,16 +13,11 @@ android {
         minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
